cba34b2438500957679c4a9a731d6eb2befb642b
lawnmower-74 <tonapricot@gmail.com>
2025-07-04 15:02:40 +0900

    Playerに走行アニメーション追加。マップ用カメラは位置固定に変更

--- Assets/Scripts/Camera/MapCamSettings.cs
+++ Assets/Scripts/Camera/MapCamSettings.cs
@@ -3,15 +3,11 @@ using UnityEngine;
 /// <summary>ミニマップ用カメラ</summary>
 public class MapCamSettings : MonoBehaviour
 {
-    public Transform TargetPlayer;
-    public Vector3 Offset = new Vector3(0, 50, 0);
+    public Vector3 CamPosition = new Vector3(0, 50, 0);
 
     void LateUpdate()
     {
-        if (TargetPlayer != null)
-        {
-            transform.position = TargetPlayer.position + Offset;
-            transform.rotation = Quaternion.Euler(90f, 0f, 0f); // 回転は固定
-        }
+        transform.position = CamPosition;
+        transform.rotation = Quaternion.Euler(90f, 0f, 0f); // 回転は固定
     }
 }
