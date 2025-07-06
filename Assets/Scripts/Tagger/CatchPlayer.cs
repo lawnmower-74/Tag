f162a6df80f848de773b21a4ad992b548380c516
lawnmower-74 <tonapricot@gmail.com>
2025-07-06 15:36:28 +0900

    鬼に捕まったときの消滅エフェクトとSEを設定

--- Assets/Scripts/Tagger/CatchPlayer.cs
+++ Assets/Scripts/Tagger/CatchPlayer.cs
@@ -3,10 +3,20 @@ using UnityEngine;
 /// <summary>Playerを捕まえ消滅させる</summary>
 public class CatchPlayer : MonoBehaviour
 {
+    public GameObject DestroyEffect;
+    public AudioClip DestroySE;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            // 消滅時エフェクト
+            GameObject playerObject = other.gameObject;
+            Instantiate(DestroyEffect, playerObject.transform.position, Quaternion.identity);
+
+            // 消滅時SE
+            AudioSource.PlayClipAtPoint(DestroySE, playerObject.transform.position);
+
             Destroy(other.gameObject);
         }
     }
