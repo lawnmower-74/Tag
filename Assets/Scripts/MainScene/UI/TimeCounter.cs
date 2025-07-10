ffa30879fb2e8edf940fdebfff9f5ee1ce039453
lawnmower-74 <tonapricot@gmail.com>
2025-07-10 15:41:09 +0900

    ゲームクリア機構の実装

--- Assets/Scripts/MainScene/UI/TimeCounter.cs
+++ Assets/Scripts/MainScene/UI/TimeCounter.cs
@@ -2,21 +2,23 @@ using System;
 using TMPro;
 using UnityEngine;
 
-/// <summary>制限時間のカウント</summary>
+/// <summary>プレイ時間のカウントダウン・ゲームクリアの管理</summary>
 public class TimeCounter : MonoBehaviour
 {
     public GameObject Player;
+    public GameObject GameClearWindow;
+    public float PlayTimeSeconds => _playTimeSeconds;
     
     private TextMeshProUGUI _timeText;
     private int _defaultPlayTimeMin = 3;
     private float _playTimeSeconds;
     private const string PLAY_TIME_KEY = "PlayTime";
 
-    private void Start()
+    void Start()
     {
         _timeText = GetComponent<TextMeshProUGUI>();
 
-        // 設定したプレイ時間を取得
+        // プレイ時間の単位換算
         int playTimeMinutes = PlayerPrefs.GetInt(PLAY_TIME_KEY, _defaultPlayTimeMin);
         _playTimeSeconds = playTimeMinutes * 60;
     }
@@ -30,10 +32,14 @@ public class TimeCounter : MonoBehaviour
         var span = new TimeSpan(0, 0, (int)_playTimeSeconds);
         _timeText.text = span.ToString(@"mm\:ss");
 
+        // 時間内、鬼から逃げ切れればゲームクリア
         if (_playTimeSeconds <= 0)
         {
-            // TODO：0秒になったときの処理
-            return;
+            Destroy(Player);
+            BGMManager.Instance.OnTaggerStopChasing();
+            
+            GameClearWindow.SetActive(true);
+            enabled = false;
         }
     }
 }
--- Assets/Scripts/TopScene/TopButtonHandler.cs
+++ Assets/Scripts/TopScene/TopButtonHandler.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 /// <summary>TOP画面のボタンに適用</summary>
-public class ButtonHandler : MonoBehaviour
+public class TopButtonHandler : MonoBehaviour
 {
     public GameObject SettingPanel;
 
+    /// <summary>プレイ時間設定用パネルの表示</summary>
     public void SetPlayTime()
     {
         SettingPanel.SetActive(true);
