c64420a5704fef34dca5a72432451aa0a156cd59
lawnmower-74 <tonapricot@gmail.com>
2025-07-12 17:12:06 +0900

    遊び方説明画面追加

--- Assets/Scripts/TopScene/TopButtonHandler.cs
+++ Assets/Scripts/TopScene/TopButtonHandler.cs
@@ -1,15 +1,27 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-/// <summary>TOP画面のボタンに適用</summary>
+/// <summary>TOPシーンのボタンに適用</summary>
 public class TopButtonHandler : MonoBehaviour
 {
+    public GameObject DescriptionPanel;
     public GameObject SettingPanel;
+
+    /// <summary>遊び方説明パネルの表示</summary>
+    public void DisplayDescriptionPanel()
+    {
+        DescriptionPanel.SetActive(true);
+    }
 
     /// <summary>プレイ時間設定用パネルの表示</summary>
     public void SetPlayTime()
     {
         SettingPanel.SetActive(true);
+    }
+
+    public void ClosePanel(GameObject panel)
+    {
+        panel.SetActive(false);
     }
 
     public void StartGame()
